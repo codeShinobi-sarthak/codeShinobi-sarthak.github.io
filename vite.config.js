55c22824a0969a040867c6e080c7e2945db84cd5
codeShinobi-sarthak <codeshinobi.sarthak@gmail.com>
2024-10-16 20:21:01 +0530

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Set the base to the repository name, as GitHub Pages serves from a subdirectory
-  base: '/codeShinobi-sarthak.github.io/',  // This is your GitHub Pages URL base
-
+  base: '/',
   plugins: [react()],
 })
